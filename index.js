33d339a9b0985f0ee565bf4dd9c27d57a28f95f8
newwrld-dev <usermmust@gmail.com>
2025-12-16 16:19:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
+let code = require('./pair'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html');
+    res.sendFile(__path + '/pair.html')
 });
 app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html');
+    res.sendFile(__path + '/main.html')
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ✅ Changed here to bind on 0.0.0.0
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`
-YOUR BOT
+Don't Forget To Give Star ‼️
 
-Server running on http://localhost:` + PORT);
+
+Server running on http://localhost:` + PORT)
 });
 
 module.exports = app;
